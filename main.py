19dc760352e36bc7ab135dfce8797e88ca090e50
mkazman3 <maxkazman@gatech.edu>
2021-06-27 15:30:52 -0400

    changed main to run old tictactoe

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import logging
 import coloredlogs
 
 from Coach import Coach
-from othello.OthelloGame import OthelloGame as Game
-from othello.pytorch.NNet import NNetWrapper as nn
+from tictactoe.TicTacToeGame import TicTacToeGame
+from tictactoe.keras.NNet import NNetWrapper as nn
+# from othello.OthelloGame import OthelloGame as Game
+# from othello.pytorch.NNet import NNetWrapper as nn
 from utils import *
 
 log = logging.getLogger(__name__)
@@ -31,7 +33,8 @@ args = dotdict({
 
 def main():
     log.info('Loading %s...', Game.__name__)
-    g = Game(6)
+    g = TicTacToeGame
+    #g = Game(6)
 
     log.info('Loading %s...', nn.__name__)
     nnet = nn(g)
